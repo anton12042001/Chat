a0ee1b44290ad82a7ff8b0a6b61c68e6e5308b81
Anton <98453976+anton12042001@users.noreply.github.com>
2022-09-11 10:31:03 +0700

    redirect if user not auth

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
+    const {email} = useSelector(state => state.user)
+    const navigate = useNavigate()
+
+
+    if(!email){
+        navigate('/authorization')
+    }
     return (
         <div>
             fasfsa
